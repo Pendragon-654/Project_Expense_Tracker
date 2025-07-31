0e3a2e8ccdfb8469b7a54ec9266de76f8b89ad9f
Pendragon-654 <abhishekburnwal654@gmail.com>
2025-08-01 00:28:20 +0530

    Fixing the image api

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -20,7 +20,9 @@ router.post("/upload-image", upload.single('image'), (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: 'No file uploaded' });
     }
-    const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+    const BASE_URL = process.env.BASE_URL || `http://localhost:${process.env.PORT || 5000}`;
+const imageUrl = `${BASE_URL}/uploads/${req.file.filename}`;
+
     res.status(200).json({ imageUrl });
 });
 
