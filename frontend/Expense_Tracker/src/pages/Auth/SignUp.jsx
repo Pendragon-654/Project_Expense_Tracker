00ba4163f3ae339c624026d7d66f9dfb379c19d5
Avishek <abhishekburnwal654@gmail.com>
2026-07-10 02:39:04 +0530

    cursor pointer in signup

--- frontend/Expense_Tracker/src/pages/Auth/SignUp.jsx
+++ frontend/Expense_Tracker/src/pages/Auth/SignUp.jsx
@@ -115,7 +115,7 @@ const SignUp = () => {
 
           {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
 
-          <button type="submit" className="btn-primary">
+          <button type="submit" className="btn-primary" hover="cursor-pointer">
             SIGN UP
           </button>
 
